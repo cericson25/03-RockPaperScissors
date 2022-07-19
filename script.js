acd46727b763d111091fe369679168f88b904b58
cericson25 <connorericsonn@gmail.com>
2022-07-18 23:54:22 -0600

    Add better UI and styling

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const buttons = document.querySelectorAll("button");
+const images = document.querySelectorAll("img");
 const playerScore = document.querySelector(".player-score");
 const computerScore = document.querySelector(".computer-score");
 const scoreSubtext = document.querySelector(".score-subtext");
 
-buttons.forEach((button) => {
-  //assigns an event listener to all buttons in nodelist
-  button.addEventListener("click", () => {
-    gameLogic(button.id, computerChoice());
+images.forEach((img) => {
+  //assigns an event listener to all images in nodelist
+  img.addEventListener("click", () => {
+    gameLogic(img.id, computerChoice());
   });
-  //calls the gameLogic function, giving the button id as the playerChoice
+  //calls the gameLogic function, giving the image id as the playerChoice
 });
 
 function computerChoice() {
@@ -30,12 +30,16 @@ function gameLogic(playerChoice, computerChoice) {
         computerScore.textContent = parseInt(computerScore.textContent) + 1;
         if (parseInt(computerScore.textContent) === 5) {
           scoreSubtext.textContent = "You lose! :(";
+          scoreSubtext.style.fontSize = "50px";
+          scoreSubtext.style.fontFamily = "RobotoBold";
         }
       } else {
         scoreSubtext.textContent = "Rock beats Scissors";
         playerScore.textContent = parseInt(playerScore.textContent) + 1;
         if (parseInt(playerScore.textContent) === 5) {
           scoreSubtext.textContent = "You win! :)";
+          scoreSubtext.style.fontSize = "50px";
+          scoreSubtext.style.fontFamily = "RobotoBold";
         }
       }
     } else if (playerChoice === "paper") {
@@ -44,6 +48,8 @@ function gameLogic(playerChoice, computerChoice) {
         playerScore.textContent = parseInt(playerScore.textContent) + 1;
         if (parseInt(playerScore.textContent) === 5) {
           scoreSubtext.textContent = "You win! :)";
+          scoreSubtext.style.fontSize = "50px";
+          scoreSubtext.style.fontFamily = "RobotoBold";
         }
       } else if (computerChoice === "paper") {
         scoreSubtext.textContent = "You both picked Paper";
@@ -52,6 +58,8 @@ function gameLogic(playerChoice, computerChoice) {
         computerScore.textContent = parseInt(computerScore.textContent) + 1;
         if (parseInt(computerScore.textContent) === 5) {
           scoreSubtext.textContent = "You lose! :(";
+          scoreSubtext.style.fontSize = "50px";
+          scoreSubtext.style.fontFamily = "RobotoBold";
         }
       }
     } else if (playerChoice === "scissors") {
@@ -60,12 +68,16 @@ function gameLogic(playerChoice, computerChoice) {
         computerScore.textContent = parseInt(computerScore.textContent) + 1;
         if (parseInt(computerScore.textContent) === 5) {
           scoreSubtext.textContent = "You lose! :(";
+          scoreSubtext.style.fontSize = "50px";
+          scoreSubtext.style.fontFamily = "RobotoBold";
         }
       } else if (computerChoice === "paper") {
         scoreSubtext.textContent = "Scissors Beats Paper";
         playerScore.textContent = parseInt(playerScore.textContent) + 1;
         if (parseInt(playerScore.textContent) === 5) {
           scoreSubtext.textContent = "You win! :)";
+          scoreSubtext.style.fontSize = "50px";
+          scoreSubtext.style.fontFamily = "RobotoBold";
         }
       } else {
         scoreSubtext.textContent = "You both picked Scissors";
@@ -73,3 +85,4 @@ function gameLogic(playerChoice, computerChoice) {
     }
   }
 } //plays one round
+// NEED TO BREAK THIS UP
